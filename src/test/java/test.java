d62da6ac8dd6efebf491c1025787c4e6f833a022
xds86 <xds_86@live.com>
2020-10-26 16:28:31 +0800

    dev

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("第一行");
         System.out.println("第二行");
+        System.out.println("dev");
 
     }
 }
